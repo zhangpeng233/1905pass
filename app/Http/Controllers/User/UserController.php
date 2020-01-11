fed135e64b4d923769b23d376a70c769641c5721
zhang <zhang@qq.com>
2020-01-11 12:35:13 +0000

    注册登录

--- app/Http/Controllers/User/UserController.php
+++ app/Http/Controllers/User/UserController.php
@@ -76,34 +76,99 @@ class UserController extends Controller
     }
       //用户登录
        public function login(Request $request)
+       {
+
+           echo '<pre>';
+           print_r($_POST);
+           echo '</pre>';
+           $value = $request->input('name');
+           $pass = $request->input('pass');
+           //按name找记录
+           $u1 = UserModel::where(['name' => $value])->first();
+           $u2 = UserModel::where(['email' => $value])->first();
+           $u3 = UserModel::where(['mobile' => $value])->first();
+           if ($u1 == NULL && $u2 == NULL && $u3 == NULL) {
+               $response = [
+                   'errno' => 400004,
+                   'msg' => "用户不存在"
+               ];
+               return $response;
+           }
+           if ($u1)     // 使用用户名登录
+           {
+               if (password_verify($pass, $u1->password)) {
+                   $uid = $u1->id;
+               } else {
+                   $response = [
+                       'errno' => 400003,
+                       'msg' => 'password wrong'
+                   ];
+                   return $response;
+               }
+           }
+           if ($u2) {        //使用 email 登录
+               if (password_verify($pass, $u2->password)) {
+                   $uid = $u2->id;
+               } else {
+                   $response = [
+                       'errno' => 400003,
+                       'msg' => 'password wrong'
+                   ];
+                   return $response;
+               }
+           }
+           if ($u3) {        // 使用电话号登录
+               if (password_verify($pass, $u3->password)) {
+                   $uid = $u3->id;
+               } else {
+                   $response = [
+                       'errno' => 400003,
+                       'msg' => 'password wrong'
+                   ];
+                   return $response;
+               }
+           }
+       }
+        protected function getToken($uid)
     {
-
-        echo '<pre>'; print_r($_POST);echo '</pre>';
-        $value = $request->input('name');
-        $pass = $request->input('pass');
-        //按name找记录
-        $u = UserModel::where(['name'=>$value])->first();
-
-        echo '<pre>'; print_r($u->toArray());echo '</pre>';
-            //验证密码
-        if(password_verify($pass,$u->password)){
-
-
-
-        }else{
-            $response =[
-                'error' => 5000004,
-                'msg'   =>'密码错误'
+        $token = md5(time() . mt_rand(11111,99999) . $uid);
+        return substr($token,5,20);
+    }
+        /**
+         * 获取用户信息接口
+         */
+        public function showTime()
+    {
+        if(empty($_SERVER['HTTP_TOKEN']) || empty($_SERVER['HTTP_UID']))
+        {
+            $response = [
+                'errno' => 40003,
+                'msg'   => 'Token Not Valid!'
             ];
             return $response;
-
-        } echo '<pre>'; print_r($u->toArray());echo '</pre>';
-
-
+        }
+        //获取客户端的 token
+        $token = $_SERVER['HTTP_TOKEN'];
+        $uid = $_SERVER['HTTP_UID'];
+        $redis_token_key = 'str:user:token:'.$uid;
+        //验证token是否有效
+        $cache_token = Redis::get($redis_token_key);
+        if($token==$cache_token)        // token 有效
+        {
+            $data = date("Y-m-d H:i:s");
+            $response = [
+                'errno' => 0,
+                'msg'   => 'ok',
+                'data'  => $data
+            ];
+        }else{
+            $response = [
+                'errno' => 40003,
+                'msg'   => 'Token Not Valid!'
+            ];
+        }
+        return $response;
     }
-
-
-
 
 
 
--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,4 @@ Route::get('/', function () {
 
 Route::post('/api/user/reg','User\UserController@reg'); //用户注册
 Route::post('/api/user/login','User\UserController@login'); //用户登录
+Route::get('/api/show/time','User\UserController@showTime'); // 获取数据
